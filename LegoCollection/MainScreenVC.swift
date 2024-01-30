e334f29ca2602f2d03340b57d7d96d98b758affc
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-30 14:49:01 +0300

    Initial Commit

---
+++ LegoCollection/MainScreenVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenVC.swift
+//  LegoCollection
+//
+//  Created by Hüseyin Kaya on 30.01.2024.
+//
+
+import UIKit
+
+class MainScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+    
+}
