beedaf895879fb542aeca21282c93555983c5a4a
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-01 14:46:00 +0300

    Create Custom Button

---
+++ LegoCollection/LCButton.swift
@@ -0,0 +1,34 @@
+//
+//  LCButton.swift
+//  LegoCollection
+//
+//  Created by Hüseyin Kaya on 1.02.2024.
+//
+
+import UIKit
+
+class LCButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) // VC'de constraints atayacagiz
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
