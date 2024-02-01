cb26eb7c84959c496ada1715d74b6cbeaaaf20f7
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-02-01 14:45:46 +0300

    Create Constants

---
+++ LegoCollection/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  LegoCollection
+//
+//  Created by Hüseyin Kaya on 1.02.2024.
+//
+
+import UIKit
+
+struct Constants {
+    static let legoRed = UIColor(named: "logoColor")
+}
